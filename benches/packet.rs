156331a7ef4b499a832f6a9f128e2abf9c894a94
苏向夜 <fu050409@163.com>
2024-05-17 15:35:29 +0800

    chore(bench): avoid some data copy in benchmarks

--- benches/packet.rs
+++ benches/packet.rs
@@ -3,10 +3,11 @@ use oblivion::{models::packet::OED, utils::generator::generate_random_salt};
 
 fn criterion_benchmark_oed(c: &mut Criterion) {
     let aes_key = generate_random_salt();
+    let long_data = vec![0u8; 1024 * 1024];
     c.bench_function("oed", |b| {
         b.iter(|| {
             OED::new(&aes_key)
-                .from_bytes([0u8; 1e6 as usize].to_vec())
+                .from_bytes(long_data.clone())
                 .unwrap()
                 .plain_data()
                 .unwrap();
