d588ff7c75970cb987973bcd9b827382d6d62708
苏向夜 <fu050409@163.com>
2024-09-19 17:13:13 +0800

    refactor(types): integrate types

--- crates/oblivion/src/types/client.rs
+++
@@ -1,1 +0,0 @@
-pub use crate::models::client::Response;
--- crates/oblivion/src/types/server.rs
+++
@@ -1,5 +0,0 @@
-use futures::future::BoxFuture;
-
-use crate::models::render::BaseResponse;
-pub type Result = BoxFuture<'static, anyhow::Result<BaseResponse>>;
-pub type Response = Result;
