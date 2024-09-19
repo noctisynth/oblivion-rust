ca5315e299a2f7019fb29db792d65d097db646aa
苏向夜 <fu050409@163.com>
2024-09-19 17:20:56 +0800

    chore(lint): fix spell check

--- crates/oblivion/benches/keygen.rs
+++ crates/oblivion/benches/keygen.rs
@@ -32,7 +32,7 @@ async fn scrypt(
 }
 
 fn criterion_benchmark_key_gen(c: &mut Criterion) {
-    c.bench_function("keygen", |b| b.iter(generate_key_pair));
+    c.bench_function("key_gen", |b| b.iter(generate_key_pair));
 }
 
 fn criterion_benchmark_kdf(c: &mut Criterion) {
